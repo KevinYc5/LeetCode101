df703004b4a0a34e3c5017606fac2ceadd508157
KevinYc5 <2689268634@qq.com>
2020-07-31 23:23:46 +0800

    Getnext dfs 布尔运算

---
+++ 6_一切皆可搜索/37.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+using namespace std;
+class Solution {
+public:
+	bitset<9> getPossibleStatus(int x, int y)
+	{
+		return ~(rows[x] | cols[y] | cells[x / 3][y / 3]);
+	}
+
+	vector<int> getNext(vector<vector<char>>& board)
+	{
+		vector<int> ret;
+		int minCnt = 10;    // 先设为最大值，通过二重循环找到可填数字最小的位置
+		for (int i = 0; i < board.size(); i++)
+		{
+			for (int j = 0; j < board[i].size(); j++)
+			{
+				if (board[i][j] != '.') continue;
+				auto cur = getPossibleStatus(i, j);
+				if (cur.count() >= minCnt) continue;
+				ret = { i, j };
+				minCnt = cur.count();
+			}
+		}
+		return ret;
+	}
+
+	void fillNum(int x, int y, int n, bool fillFlag)
+	{
+		rows[x][n] = (fillFlag) ? 1 : 0;
+		cols[y][n] = (fillFlag) ? 1 : 0;
+		cells[x/3][y/3][n] = (fillFlag) ? 1: 0;
+	}
+
+	bool dfs(vector<vector<char>>& board, int cnt)
+	{
+		if (cnt == 0) return true;
+
+		auto next = getNext(board);
+		auto bits = getPossibleStatus(next[0], next[1]);
+		for (int n = 0; n < bits.size(); n++)
+		{
+			if (!bits.test(n)) continue;
+			fillNum(next[0], next[1], n, true);
+			board[next[0]][next[1]] = n + '1';
+			if (dfs(board, cnt - 1)) return true;
+			board[next[0]][next[1]] = '.';  // 回改
+			fillNum(next[0], next[1], n, false); // 回改
+		}
+		return false;
+	}
+
+	void solveSudoku(vector<vector<char>>& board)
+	{
+		rows = vector<bitset<9>>(9, bitset<9>());
+		cols = vector<bitset<9>>(9, bitset<9>());
+		cells = vector<vector<bitset<9>>>(3, vector<bitset<9>>(3, bitset<9>()));
+
+		int cnt = 0;
+		for (int i = 0; i < board.size(); i++)
+		{
+			for (int j = 0; j < board[i].size(); j++)
+			{
+				cnt += (board[i][j] == '.');
+				if (board[i][j] == '.') continue;
+				int n = board[i][j] - '1';
+				rows[i] |= (1 << n);    // 布尔并
+				cols[j] |= (1 << n);
+				cells[i / 3][j / 3] |= (1 << n);
+			}
+		}
+		dfs(board, cnt);
+	}
+
+private:
+	vector<bitset<9>> rows;
+	vector<bitset<9>> cols;
+	vector<vector<bitset<9>>> cells;
+};
+
+//
+// Created by Kevin on 2020/7/30 16:23.
+// 求真求实，大气大为
+//
+
